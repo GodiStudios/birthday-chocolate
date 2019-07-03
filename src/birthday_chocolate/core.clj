00176b56415d707b05ba3ad85290a5925ab61f72
naomijub <jnaomi@thoughtworks.com>
2019-07-02 23:19:44 -0300

    Resolving this shit

--- src/birthday_chocolate/core.clj
+++ src/birthday_chocolate/core.clj
@@ -1,7 +1,27 @@
 (ns birthday-chocolate.core
   (:gen-class))
 
+(defn chocolate-bar-generator []
+  (let [rand-vec (atom [])]
+    (dotimes [x (rand-int 100)] 
+      (swap! rand-vec conj (+ 1 (rand-int 5))))
+    @rand-vec))
+
+(defn birthday-generator []
+  {:day (+ 1 (rand-int 31)) :month (+ 1 (rand-int 12))})
+
+(defn share-chocolate [vec birth]
+  (->> vec
+    (partition (:month birth) 1)
+    (filter #(= (reduce + 0 %) (:day birth)))
+    (count)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [vec (chocolate-bar-generator)
+        birth (birthday-generator)]
+    (do
+      (println vec)
+      (println birth)
+      (println (share-chocolate vec birth)))))
--- test/birthday_chocolate/core_test.clj
+++ test/birthday_chocolate/core_test.clj
@@ -2,6 +2,24 @@
   (:require [clojure.test :refer :all]
             [birthday-chocolate.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest generate-chocolate-bar
+  (testing "Testing generated array"
+    (is (< (count (chocolate-bar-generator)) 100))
+    (is (every? (partial >= 5) (chocolate-bar-generator)))
+    (is (every? (partial <= 1) (chocolate-bar-generator)))))
+
+(deftest generate-birthday 
+  (testing "Returns a map with day/month"
+    (is (= (keys (birthday-generator)) '(:day :month)))
+    (testing "Day of birth"
+      (is (and (>= (:day (birthday-generator)) 1)
+               (<= (:day (birthday-generator)) 31))))
+    (testing "Month of birth"
+      (is (and (>= (:month (birthday-generator)) 1)
+               (<= (:month (birthday-generator)) 12))))))
+
+(deftest chocolate-can-be-shared
+  (testing "chocolate bars are shared by num of tablets equal month and sum of tablets equal day"
+    (is (= (share-chocolate [1 2 1 3 2] {:day 3 :month 2}) 2))
+    (is (= (share-chocolate [1 1 1 1 1 1] {:day 3 :month 2}) 0))
+    (is (= (share-chocolate [4] {:day 4 :month 1}) 1))))
