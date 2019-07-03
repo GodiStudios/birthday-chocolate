31b44dda67731507429c3fb6e82ced6c2893bd47
naomijub <jnaomi@thoughtworks.com>
2019-07-02 22:31:57 -0300

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to birthday-chocolate
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/birthday_chocolate/core_test.clj
@@ -0,0 +1,7 @@
+(ns birthday-chocolate.core-test
+  (:require [clojure.test :refer :all]
+            [birthday-chocolate.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
